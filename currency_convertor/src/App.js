84df7657fad14a2b9788136c2eff3f39122708cb
Shubhi Agrawal <shubhi.agrawal_cs19@gla.ac.in>
2024-01-03 16:21:15 +0530

    Created app

--- currency_convertor/src/App.js
+++ currency_convertor/src/App.js
@@ -1,11 +1,103 @@
+import { useEffect, useState } from 'react';
+// import CurrencyInput from "react-currency-input-field";
 import './App.css';
 
 function App() {
+  const [rates, setRates] = useState();
+  const [ratesFetched, setRatesFetched] = useState(false);
+  const [amount, setAmount] = useState(0);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("USD");
+  const [output, setOutput] = useState();
+  
+  const getRates = async () => {
+    // fetch the data from API
+    const response = await fetch(
+      "https://v6.exchangerate-api.com/v6/59d560835c4e8d4f996adc83/latest/USD"
+    ).then((response) => response.json());
+
+    // save the rates in the state
+    if (response.result === "success") {
+      setRates(response.conversion_rates);
+      setRatesFetched(true);
+    }
+  };
+
+  useEffect(() => {
+    getRates();
+  }, []);
+
+  const calculateOutput = async () => {
+    // fetch the selected from currency rates
+    const response = await fetch(
+      `https://v6.exchangerate-api.com/v6/59d560835c4e8d4f996adc83/latest/${fromCurrency}`
+    ).then((response) => response.json());
+    const fetchedRates = response.conversion_rates;
+    const CurrencyRate = fetchedRates[toCurrency];
+    const output = amount * CurrencyRate;
+    setOutput(output);
+  };
+
   return (
-    <div className="App">
-      This is a task
+    <div className="container">
+      <div className='box'>
+      <h4>Crypto-Convertor</h4>
+      <div className="input-amount">
+        <label>Amount:</label>
+        <input
+          value={amount && Math.max(0, amount)}
+          onChange={(e) => setAmount(e.target.value ? Number(e.target.value) : e.target.value)}
+          type='number'
+        />
+      </div>
+
+      <div className="input-from">
+        <label>From:</label>
+        <select
+          id="from"
+          value={fromCurrency}
+          onChange={(e) => setFromCurrency(e.target.value)}
+        >
+          {ratesFetched ? (
+            Object.keys(rates).map((currency, index) => (
+              <option key={index} value={currency}>
+                {currency}
+              </option>
+            ))
+          ) : (
+            <option defaultValue>USD</option>
+          )}
+        </select>
+      </div>
+
+      <div className="input-to">
+        <label>To:</label>
+        <select
+          id="to"
+          value={toCurrency}
+          onChange={(e) => setToCurrency(e.target.value)}
+        >
+          {ratesFetched ? (
+            Object.keys(rates).map((currency, index) => (
+              <option key={index} value={currency}>
+                {currency}
+              </option>
+            ))
+          ) : (
+            <option defaultValue>EUR</option>
+          )}
+        </select>
+      </div>
+      <button className="btn" onClick={() => calculateOutput()}>
+        Calculate
+      </button>
+      <div className="output">
+        <label>Output: {output}</label>
+      </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
